a1d798262fc3dd676d4414ff3c38c12ef3691afa
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-03-04 12:40:55 +0530

    Create Implement stack using array.java

---
+++ GFG/Arrays/Implement stack using array.java
@@ -0,0 +1,53 @@
+class myStack {
+    int arr[];
+    int top;
+
+    public myStack(int n) {
+        // Define Data Structures.
+        arr=new int[n];
+        top=-1;
+        
+    }
+
+    public boolean isEmpty() {
+        // check if the stack is empty
+        if(top==-1){
+            return true;
+        }
+        return false;
+        
+    }
+
+    public boolean isFull() {
+        // check if the stack is full
+        if(top==arr.length-1){
+            return true;
+        }
+        return false;
+        
+    }
+
+    public void push(int x) {
+        // Inserts x at the top of the stack.
+        if(isFull()){
+            return;
+        }
+        arr[++top]=x;
+    }
+
+    public void pop() {
+        // Removes an element from the top of the stack
+        if(isEmpty()){
+            return;
+        }
+        top--;
+    }
+
+    public int peek() {
+        // Returns the top element of the stack
+        if(isEmpty()){
+            return -1;
+        }
+        return arr[top];
+    }
+}
