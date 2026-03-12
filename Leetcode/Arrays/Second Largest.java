a36be8964762fa636e3fb34063601d676310ca0e
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-03-12 23:29:05 +0530

    Create Second Largest.java

---
+++ Leetcode/Arrays/Second Largest.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here.
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+        for(int n:arr){
+            if(max < n){
+                smax=max;
+                max=n;
+            }
+            else if(smax < n && n!=max){
+                smax= n;
+            }
+        }
+        if(smax == Integer.MIN_VALUE){ // NO second maximum.
+            return -1;
+        }
+        return smax;
+    }
+}
+TC:O(n)
+SC:O(n)
