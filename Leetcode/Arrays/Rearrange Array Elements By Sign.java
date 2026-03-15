84f60bea8fb5f7621ab9f94aba57b169dc128c8c
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-03-15 23:20:24 +0530

    Create Rearrange Array Elements By Sign.java

---
+++ Leetcode/Arrays/Rearrange Array Elements By Sign.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        int e=0;
+        int o=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0){
+                ans[o]=nums[i];
+                o+=2;
+            }
+            else{
+                ans[e]=nums[i];
+                e+=2;
+            }
+        } 
+        return ans;
+    }
+}
