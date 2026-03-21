f9431cef04a02d8a80b655c9785b817ec1a74c05
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-03-22 02:47:15 +0530

    Create Kth Largest Element in an Array Brute Force.java

---
+++ Leetcode/Heaps/Kth Largest Element in an Array Brute Force.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer>pq = new PriorityQueue<>( (a,b) -> b-a );
+        for(int n:nums){
+            pq.add(n);
+        }
+        for(int i=1;i<k;i++){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
+TC:O(NlogN + klogN)
+SC:O(N)
