de6495d82eaca7b37296ed29cfbea5a40204d24a
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-03-25 23:08:15 +0530

    Create  Majority Element.java

---
+++ Leetcode/Arrays/Majority Element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ele=0;
+        int cnt=0;
+        for(int n:nums){
+            if(cnt==0){//we're starting with curr n
+                 cnt++;
+                 ele=n;
+            }
+            else if(n==ele){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        return ele;
+    }
+}
