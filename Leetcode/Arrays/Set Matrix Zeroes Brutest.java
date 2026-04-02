f46cb91b14e65dd3ed4b41d10efec373ebd79519
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-04-02 22:45:43 +0530

    Create  Set Matrix Zeroes Brutest.java

---
+++ Leetcode/Arrays/Set Matrix Zeroes Brutest.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    public void colZero(int temp[][], int r , int c){
+        for(int i=0;i<temp.length;i++){
+            temp[i][c] = 0;
+        }
+    }
+    public void rowZero(int temp[][], int r , int c){
+        for(int i=0;i<temp[0].length;i++){
+            temp[r][i] = 0;
+        }
+    }
+    public void setZeroes(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int temp[][] = new int[r][c];
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                temp[i][j] = -1;
+            }
+        }
+        
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==0){
+                    colZero(temp, i, j);
+                    rowZero(temp, i, j);
+                }
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(temp[i][j]==0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+TC:O(rc(r+c))
+SC:O(rc)
