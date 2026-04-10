b86866b34333e7f2c27f02a6fc892ddc3894c43c
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-04-10 23:15:58 +0530

    Create Swap two numbers.java

---
+++ GFG/Basic/Swap two numbers.java
@@ -0,0 +1,9 @@
+class Solution {
+    static List<Integer> get(int a, int b) {
+        // code here...
+        List<Integer>ans=new ArrayList<>();
+        ans.add(b);
+        ans.add(a);
+        return ans;
+    }
+}
