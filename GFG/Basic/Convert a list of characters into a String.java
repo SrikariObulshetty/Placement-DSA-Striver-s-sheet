814796a8ac1ae917426b9790ff18f7f686e61b38
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-04-17 23:08:28 +0530

    Add method to convert char array to String
    Implement a method to convert a character array to a String using StringBuilder.

---
+++ GFG/Basic/Convert a list of characters into a String.java
@@ -0,0 +1,11 @@
+// User function Template for Java.....
+
+class Solution {
+    public String chartostr(char arr[], int N) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++){
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+}
