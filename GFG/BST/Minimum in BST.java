0633c752895624c404526e38d88d3c52ddcacadb
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-05-16 23:22:49 +0530

    Create Minimum in BST.java

---
+++ GFG/BST/Minimum in BST.java
@@ -0,0 +1,22 @@
+/*
+Definition for Node
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public int minValue(Node root) {
+        // code here.....
+        if(root.left==null){
+            return root.data;
+        }
+        return minValue(root.left);
+    }
+}
