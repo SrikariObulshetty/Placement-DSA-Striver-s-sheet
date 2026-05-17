1bf9e51e84bccc4c8aa5deb1745c62fb93b8e27e
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-05-17 17:36:56 +0530

    Create Delete Node in a BST.java

---
+++ Leetcode/BST/Delete Node in a BST.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+        else if(root.val < key){
+            root.right = deleteNode(root.right, key);
+        }
+        else if(root.val> key){
+             root.left = deleteNode(root.left, key);
+        }
+        else{ // Found the node
+             //CASES
+            if(root.left==null){
+                root=root.right;
+             }
+             else if(root.right==null){
+                root=root.left;
+             }       
+             else{ //It is internal node.
+                 TreeNode temp = root.right;
+                 while(temp.left!=null){
+                    temp=temp.left;
+                 }
+                 root.val=temp.val;
+                 root.right = deleteNode(root.right,temp.val);
+             }
+        }
+        return root;
+    }
+}
