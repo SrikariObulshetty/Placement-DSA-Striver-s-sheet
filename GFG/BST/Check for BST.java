238ff3647459ecb55e92205a9a21b6cbb370a198
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-05-18 23:24:54 +0530

    Create Check for BST.java

---
+++ GFG/BST/Check for BST.java
@@ -0,0 +1,30 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public boolean helper(Node root, int min, int max){
+        if(root==null){
+            return true;
+        }
+        if(root.data<=min || root.data>=max){
+            return false;
+        }
+        return helper(root.left, min, root.data) && helper(root.right, root.data, max);
+    }
+    
+    public boolean isBST(Node root) {
+        // code here.....
+        return helper(root,Integer.MIN_VALUE, Integer.MAX_VALUE);
+        
+    }
+}
