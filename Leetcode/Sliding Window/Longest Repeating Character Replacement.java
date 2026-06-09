299ac098959ca38dd4ee077c76c3ece4a4efc0a0
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-06-09 16:33:49 +0530

    Create  Longest Repeating Character Replacement.java

---
+++ Leetcode/Sliding Window/Longest Repeating Character Replacement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+         //Brute Force.
+          int l = s.length();
+          int ans = 0;
+          int arr[] = new int[26];
+          int maxFreq=0;
+          int i=0,j=0;
+          while(j < l){
+             arr[s.charAt(j) - 'A']++;
+             maxFreq = Math.max(maxFreq, arr[s.charAt(j) - 'A']);
+             while(j-i+1 - maxFreq > k){
+                arr[s.charAt(i++) - 'A']--;
+                maxFreq=0;
+                for(int m=0;m<26;m++){
+                     maxFreq = Math.max(maxFreq, arr[m]);
+                }
+             }
+             ans=Math.max(ans, j-i+1);
+             j++;
+          }
+          return ans;
+    }
+}
