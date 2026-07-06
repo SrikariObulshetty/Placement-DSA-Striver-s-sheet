4f53f53ecd69dca5e93622b1019429d61ff3d029
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-07-07 00:13:34 +0530

    Create Climbing Stairs Using Space Optimisation.java

---
+++ Leetcode/DP/Climbing Stairs Using Space Optimisation.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        int f = 1;
+        int s = 2;
+        for(int i=3;i<=n;i++){
+           int ans = f + s;
+            f  = s;
+            s = ans;
+        }
+        return s; 
+    }
+}
