7ab8d7ba7b229f707cb4ac107038578885f193ad
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-09-03 23:08:53 +0530

    Create House Robber Using space optimisation.java

---
+++ Leetcode/DP/House Robber Using space optimisation.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        else if(n==2){
+            return Math.max(nums[0], nums[1]); 
+        }
+        int f = nums[0];
+        int s = Math.max(nums[0], nums[1]);
+        for(int i=2;i<n;i++){
+           int t1 = nums[i] + f;
+           f = s;
+           s = Math.max(t1, s);
+        }
+        return s;  
+    }
+}
