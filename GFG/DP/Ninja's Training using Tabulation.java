e9836d5db1ffd18788ad1580fab8a0486d04cb8a
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-09-08 11:53:14 +0530

    Update Ninja's Training using Tabulation.java

--- GFG/DP/Ninja's Training using Tabulation.java
+++ GFG/DP/Ninja's Training using Tabulation.java
@@ -1,24 +1,5 @@
 class Solution {
-    
-    
-    public int memo(int mat[][], int day, int days, int prev, int[][] dp){
-        if(day >= days){
-            return 0;
-        }
-        if(dp[day][prev] != -1){
-            return dp[day][prev];
-        }
-        
-        int sum =  Integer.MIN_VALUE;
-        for(int i = 0; i<3; i++){
-            if(i!=prev){ 
-                sum = Math.max(sum, mat[day][i] + memo(mat, day+1, days, i,dp)); 
-            }
-            
-        } 
-        dp[day][prev] = sum;
-        return sum;  
-    } 
+
     
     public int maximumPoints(int mat[][]) {
         // code here.....
@@ -34,14 +15,14 @@ class Solution {
                 for(int k=0;k<3;k++){
                     if(j!=k){
                         max=Math.max(max, mat[i][j] + dp[i-1][k]); 
-                    }
-                    
+                    }  
+                     
                 }
                 dp[i][j]=max; 
                 dp[i][3] = Math.max(dp[i][3], max);
                  
             } 
-             
+              
         }
         return dp[days-1][3]; 
         
