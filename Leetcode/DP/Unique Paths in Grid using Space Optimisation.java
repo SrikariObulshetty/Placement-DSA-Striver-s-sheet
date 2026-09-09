9aa33ba12471558f40806c3afa92c2bcdaa2f27b
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-09-10 04:37:22 +0530

    Create Unique Paths in Grid using Space Optimisation.java

---
+++ Leetcode/DP/Unique Paths in Grid using Space Optimisation.java
@@ -0,0 +1,27 @@
+class Solution {
+
+
+    public int uniquePaths(int m, int n) {
+         int dp[] = new int[m];
+         for(int i=0;i<m;i++){
+            int temp[] = new int[n];
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    temp[0] = 1;
+                    continue;
+                } 
+                int up = 0;
+                int left = 0;
+                if(i >= 1){
+                    up = dp[j]; 
+                }
+                if(j >= 1){
+                    left = temp[j-1]; 
+                }
+                temp[j] = up + left; 
+            }
+            dp = temp;
+         }
+         return dp[n-1]; 
+    }
+}
