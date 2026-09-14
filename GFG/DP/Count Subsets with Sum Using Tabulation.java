57535bd494ea13e87e76d5852ffde5916257b07e
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-09-14 19:15:48 +0530

    Create Count Subsets with Sum Using Tabulation.java

---
+++ GFG/DP/Count Subsets with Sum Using Tabulation.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    
+  
+    static int perfectSum(int[] arr, int target) {
+        // code here.....
+        int n = arr.length;
+        int dp[][] = new int[n][target + 1];
+        
+        for(int i=0;i<n;i++){
+            dp[i][0] = 1;
+            
+        }
+        
+        if(arr[0] <= target){ 
+            dp[0][arr[0]] += 1; 
+        } 
+        
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){ 
+                
+               dp[i][j] = dp[i-1][j];
+                
+                if(arr[i] <= j){ 
+                    dp[i][j] += dp[i-1][j - arr[i]];
+                    
+                }
+                
+            }
+        }
+        
+        
+        return dp[n-1][target];
+        
+    }
+}
