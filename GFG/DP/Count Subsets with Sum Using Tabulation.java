22d69b11bf63b6daffac9db04d07a5e92064ad4f
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-09-14 19:16:54 +0530

    Update Count Subsets with Sum Using Tabulation.java

--- GFG/DP/Count Subsets with Sum Using Tabulation.java
+++ GFG/DP/Count Subsets with Sum Using Tabulation.java
@@ -13,7 +13,7 @@ class Solution {
         }
         
         if(arr[0] <= target){ 
-            dp[0][arr[0]] += 1; 
+            dp[0][arr[0]] += 1;  // I did not get this case to add one, I thought dp[0][arr[0]] = 1;, which goes wrong if target is 0
         } 
         
         
